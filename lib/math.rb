03366cb26b54e6191b09b00bccd36531eddfcd59
Corina Talavera <corina.talavera@gmail.com>
2018-10-17 11:49:17 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,11 +7,11 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  puts 10/2
+  puts 10/2=5
 end
 
 def multiplication(num1, num2)
-  puts 10*2
+  puts 10*2=20
 end
 
 def modulo(num1, num2)
