f2aacf84cfc9ece6d1ba0ec42bd6fd885140c426
Corina Talavera <corina.talavera@gmail.com>
2018-10-17 11:51:54 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(num1, num2)
+puts 1+1
 end
 
 def subtraction(num1, num2)
+ puts  1-1=0
 end
 
 def division(num1, num2)
+  puts 10/2=5
 end
 
 def multiplication(num1, num2)
+  puts 10*2=20
 end
 
 def modulo(num1, num2)
+  5%3=2
 end
 
 def square_root(num)
+  puts math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  puts 1+((2*2)/2)=3
 end
